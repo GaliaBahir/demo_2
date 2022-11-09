e5419654bbd67babd1a1dc076e62f761463df638
GaliaBahir <galiab@codevalue.com>
2022-11-09 20:18:56 +0200

    Added admin

--- store/admin.py
+++ store/admin.py
@@ -6,3 +6,11 @@ from .models import Category, Product
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
     prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'slug', 'price',
+                    'in_stock', 'created', 'updated']
+    list_filter = ['in_stock', 'is_active']
+    list_editable = ['price', 'in_stock']
+    prepopulated_fields = {'slug': ('title',)}

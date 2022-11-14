9bcc47f7e112e50069d7e606baaadf2cd81b6a40
GaliaBahir <galiab@codevalue.com>
2022-11-14 08:25:22 +0200

    Removed unncessary fields

--- store/models.py
+++ store/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True)
-    slug = models.SlugField(max_length=255, unique=True)
 
 
     class Meta:
@@ -18,7 +17,6 @@ class Product(models.Model):
     author = models.CharField(max_length=255, default='author')
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='images/')
-    slug = models.SlugField(max_length=255)
     price = models.DecimalField(max_digits=4, decimal_places=2)
     in_stock = models.BooleanField(default=True)
     is_active = models.BooleanField(default=True)
